315408b85bb15f1a455d4af8ef4e83d2034faa72
davejmac <david.mcnamara@outlook.com>
2015-10-08 11:31:23 -0400

    initialize package

---
+++ django-easypost/README.md
@@ -0,0 +1,19 @@
+# django-easypost
+
+## Usage
+
+
+## Testing
+
+`python runtests.py `
+
+
+## Development
+
+```
+# to get easypost on the python sys path (should be in a virtualenv for this project)
+pip install -e .
+
+# For model migrations
+django-admin makemigrations easypost --settings=easypost.test_settings
+```
---
+++ django-easypost/easypost/test_settings.py
@@ -0,0 +1,30 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+USE_TZ = True
+
+MIDDLEWARE_CLASSES = ()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Use a fast hasher to speed up tests.
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
+SECRET_KEY = 'test-key'
+
+ROOT_URLCONF = 'easypost.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'easypost',
+]
+
+SITE_ID = 1
