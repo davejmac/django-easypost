9283225f64ca8b51b8cdf23456b7359885d7f4b9
davejmac <david.mcnamara@outlook.com>
2015-10-19 15:12:37 -0400

    add EasyPost models to the django admin

--- easypost/admin.py
+++ easypost/admin.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+
+from .models import Address, Shipment, ShipmentItem, Parcel, ShipmentTrackingHistory
+
+
+admin.site.register(Address)
+admin.site.register(Shipment)
+admin.site.register(ShipmentItem)
+admin.site.register(Parcel)
+admin.site.register(ShipmentTrackingHistory)
