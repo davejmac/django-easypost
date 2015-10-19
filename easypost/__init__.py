bae8ea62ff6e1bfe905d9c47b7d912cb6b7bd78a
davejmac <david.mcnamara@outlook.com>
2015-10-19 15:35:50 -0400

    add project metadata to init

--- easypost/__init__.py
+++ easypost/__init__.py
@@ -1,1 +1,15 @@
-__version__ = '0.1.0'
+from __future__ import unicode_literals
+
+
+__title__ = "django-easypost"
+__summary__ = "A django wrapper for the python easypost library"
+__uri__ = "https://github.com/davejmac/django-easypost"
+
+__version__ = "0.0.1"
+
+__author__ = "Dave McNamara"
+__email__ = "david.mcnamara@outlook.com"
+
+__license__ = "MIT"
+__license__ = "License :: OSI Approved :: MIT License"
+__copyright__ = "Copyright 2015"
