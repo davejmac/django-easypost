6aae44fe94c8861e73aedd29ffaf9e1ef4b0839d
davejmac <david.mcnamara@outlook.com>
2015-10-19 15:13:18 -0400

    set up url and view for EasyPost web hook

--- easypost/urls.py
+++ easypost/urls.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('easypost.views',
+                       url(r'^webhook/$', 'easypost_webhook_callback', name='easypost_webhook_callback'),
+)
--- easypost/views.py
+++ easypost/views.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+
+from .tasks import process_webhook_event
+
+
+@csrf_exempt
+def easypost_webhook_callback(request):
+    """
+    Handle webhook callbacks from EasyPost
+    """
+
+    if request.method == 'POST':
+        post_content = request.body
+        # responses need to be 30 seconds or less, so rather than processing here, this just verifies that JSON could be decoded
+        # and then pushes it off to an asynchronous task
+        process_webhook_event.delay(post_content)
+        return HttpResponse()
+
+    return HttpResponseNotAllowed(['POST', ])
