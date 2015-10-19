dc08acc25e5e7d627d52a69f189f5284cb408058
davejmac <david.mcnamara@outlook.com>
2015-10-19 15:46:32 -0400

    add back in SECRET_KEY

--- easypost/test_settings.py
+++ easypost/test_settings.py
@@ -16,7 +16,8 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
 
-EASYPOST_API_KEY = 'test-key'
+SECRET_KEY = 'fake-key'
+EASYPOST_API_KEY = 'test-key'  # for testing, set this key to the test key that EasyPost generates for your account
 
 ROOT_URLCONF = 'easypost.urls'
 
